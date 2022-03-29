cf7988498e71b2ae9b6db8fa65d756fbbff2002a
E=mc2@6.18 <ali.alkaraawi@crowdbotics.com>
2022-03-29 14:55:34 +0300

    using onclick

--- src/app/video_list.rs
+++ src/app/video_list.rs
@@ -15,19 +15,44 @@ pub struct RenderedAtProps {
 }
 
 
+#[derive(Properties, PartialEq)]
+pub struct StateProps {
+    pub title: String,
+    pub description: String,
+    pub id: usize,
+    pub url: String,
+}
+
+
+#[function_component(UseState)]
+pub fn state(props: &StateProps) -> Html {
+    let counter = use_state(|| false);
+    let onclick = {
+        let counter = counter.clone();
+        Callback::from(move |e: MouseEvent| counter.set(!*counter))
+    };
+
+    html! {
+<div class="card mb-3" style="max-width: 18rem;">
+<div {onclick}  class="card-title" key={props.id}>{ format!("Title: {}!",props.title) }</div>
+  if *counter {
+        <div class="card-text" key={props.id}>{ format!("speaker: {}!",props.description) }</div>
+    }
+<a href={format!("{}",props.url)} class="btn btn-primary" key={props.id}>{ "watch now"}</a>
+</div>
+}
+}
+
 #[function_component(Videos)]
 pub fn videos(props: &RenderedAtProps) -> Html {
     html! {
     <>
+
     <p>{&props.title}</p>
-    <div id="introductions">
+    <div  id="introductions">
         {
              props.videos.clone().into_iter().map(|name| {
-                html!{<div class="card mb-3" style="max-width: 18rem;">
-                <div  class="card-title" key={name.id}>{ format!("Title: {}!",name.title) }</div>
-                <div class="card-text" key={name.id}>{ format!("speaker: {}!",name.speaker) }</div>
-                <a href={format!("{}",name.url)} class="btn btn-primary" key={name.id}>{ "watch now"}</a>
-                </div>}
+                html!{<UseState id=1 title={name.title} description={name.speaker} url={name.url}  />}
             }).collect::<Html>()
         }
     </div>
