d79fe22dcb675e6dfbfcfe1d0433dbc3c2013b91
E=mc2@6.18 <ali.alkaraawi@crowdbotics.com>
2022-03-30 12:28:21 +0300

    adding tailwindcomponents

--- src/app.rs
+++ src/app.rs
@@ -1,45 +1,7 @@
 use yew::prelude::*;
 use html_minifier::HTMLMinifier;
-use std::ops::Deref;
 
 
-// fn inheritance_of_text_area(text_area: HtmlTextAreaElement) {
-//     // HtmlTextAreaElement is <textarea> in html.
-//     let html_element: &HtmlElement = text_area.deref();
-//
-//     let element: &Element = html_element.deref();
-//
-//     let node: &Node = element.deref();
-//
-//     let event_target: &EventTarget = node.deref();
-//
-//     // Notice we've moved from web-sys types now into built-in
-//     // JavaScript types which are in the js-sys crate.
-//     let object: &js_sys::Object = event_target.deref();
-//
-//     // Notice we've moved from js-sys type to the root JsValue from
-//     // the wasm-bindgen crate.
-//     let js_value: &wasm_bindgen::JsValue = object.deref();
-//
-//     // Using deref like this means we have to manually traverse
-//     // the inheritance tree, however, you can call JsValue methods
-//     // on the HtmlTextAreaElement type.
-//     // The `is_string` method comes from JsValue.
-//     assert!(!text_area.is_string());
-//
-//     // empty function just to prove we can pass HtmlTextAreaElement as a
-//     // &EventTarget.
-//     fn this_function_only_takes_event_targets(targets: &EventTarget) {};
-//
-//     // The compiler will walk down the deref chain in order to match the types here.
-//     this_function_only_takes_event_targets(&text_area);
-//
-//     // The AsRef implementations allow you to treat the HtmlTextAreaElement
-//     // as an &EventTarget.
-//
-//     let event_target: &EventTarget = text_area.as_ref();
-//
-// }
 
 
 #[function_component(App)]
@@ -49,14 +11,27 @@ pub fn app() -> Html {
             e.prevent_default();
         }
     });
-    let window: web_sys::Window = web_sys::window().expect("window not available");
-    window.alert_with_message("hello from wasm!").expect("alert failed");
+    // let window: web_sys::Window = web_sys::window().expect("window not available");
+    // window.alert_with_message("hello from wasm!").expect("alert failed");
 
 
     html! {
     <>
+    <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+  <div class="flex justify-center md:justify-end -mt-16">
+    <img class="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"/>
+  </div>
+  <div>
+    <h2 class="text-gray-800 text-3xl font-semibold">{"Hello tailwindui"}</h2>
+    <p class="mt-2 text-gray-600">{"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!"}</p>
+  </div>
+  <div class="flex justify-end mt-4">
+    <a href="#" class="text-xl font-medium text-indigo-500">{"John Doe"}</a>
+  </div>
+  </div>
+
     <h1 onkeydown={&onkeydown} contenteditable="true" >{"title"}</h1>
-    <p  onkeydown={&onkeydown} clas="editable" data-placeholder="Edit me..." contenteditable="true" >{ "paragraph" }</p>
+    <p  onkeydown={&onkeydown} clas="editable" data-placeholder="Edit me..." contenteditable="true" >{ "" }</p>
     </>
     }
 }
